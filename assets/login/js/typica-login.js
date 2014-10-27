50d1cb8d1ebbbfbacc59303883fc137ac58b1f9e
adeluleye <adelu4real@gmail.com>
2014-10-27 13:45:10 +0000

    hack4oil

---
+++ assets/login/js/typica-login.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($) {
+
+	$.backstretch([
+      "assets/images/login/bg1.png", 
+      "assets/images/login/bg2.png"
+  	], {duration: 3000, fade: 750});
+		
+});
+
+
---
+++ js2/typica-login.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($) {
+
+	$.backstretch([
+      "Images/bg1.png", 
+      "Images/bg2.png"
+  	], {duration: 3000, fade: 750});
+		
+});
+
+
